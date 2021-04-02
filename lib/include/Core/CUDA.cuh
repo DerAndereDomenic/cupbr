1d489991ed25b6af1c0000f756fbc2c6b58750e7
Domenic Zingsheim <domeniczingsheim@gmail.com>
2021-04-02 14:20:16 +0000

    Add a cuda safe call function

--- lib/include/Core/CUDA.cuh
+++ lib/include/Core/CUDA.cuh
@@ -1,7 +1,20 @@
 #ifndef __CUPBR_CORE_CUDA_CUH
 #define __CUPBR_CORE_CUDA_CUH
 
+#include <iostream>
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+
+inline
+void check(cudaError_t error, char const *const func, const char *const file, int const line)
+{
+    if(error != cudaSuccess)
+    {
+        fprintf(stderr, "CUDA error at %s:%d code=%d(%s) \"%s\" \n", file, line,
+            static_cast<unsigned int>(error), cudaGetErrorString(error), func);
+    }
+}
+
+#define cudaSafeCall(val) check((val), #val, __FILE__ __LINE__)
 
 #endif
