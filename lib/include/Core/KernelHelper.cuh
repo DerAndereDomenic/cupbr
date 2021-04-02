6bf7dbed743dc8c95184d4fa6e2d7a0c9fec6b2c
Domenic Zingsheim <domeniczingsheim@gmail.com>
2021-04-02 00:04:56 +0000

    Add a class to manage kernels and threads

---
+++ lib/include/Core/KernelHelper.cuh
@@ -0,0 +1,67 @@
+#ifndef __CUPBR_CORE_KERNELHELPER_CUH
+#define __CUPBR_CORE_KERNELHELPER_CUH
+
+#include <Math/Vector.h>
+
+/**
+*   @brief A namespace that capsules thread helper functions
+*/
+namespace ThreadHelper
+{
+    /**
+    *   @brief Get the thread index of a kernel thread
+    *   @return The thread index
+    */
+    __device__
+    uint32_t 
+    globalThreadIndex();
+    
+    /**
+    *   @brief Convertes a 1D index to a 2D pixel
+    *   @param[in] index The index
+    *   @param[in] width The width of the image
+    *   @param[in] height The height of the image
+    *   @return The corresponding pixel
+    */
+    __host__ __device__
+    Vector2uint32_t 
+    index2pixel(const uint32_t& index, const uint32_t& width, const uint32_t& height);
+    
+    /**
+    *   @brief Converts a 2D pixel to a 1D index
+    *   @param[in] pixel The pixel
+    *   @param[in] width The width of the image
+    *   @return The corresponding index
+    */
+    __host__ __device__
+    uint32_t 
+    pixel2index(const Vector2uint32_t& pixel, const uint32_t& width);
+}
+
+/**
+*   @brief A namespace for managing kernel sizes
+*/
+namespace KernelSizeHelper
+{
+    
+    /**
+    *   @brief A namespace for Kernel sizes
+    */
+    struct KernelSize
+    {
+        const uint32_t THREADS_PER_BLOCK = 128;     /**< The number of threads per block */
+        uint32_t blocks;                            /**< The number of blocks */
+        uint32_t threads;                           /**< The number of threads */
+    };
+    
+    /**
+    *   @brief Constructs the block/thread size needed for the given size
+    *   @param[in] size The overall number of threads needed
+    *   @note The returned blocks*threads >= size
+    */
+    __host__
+    KernelSize configure(const uint32_t& size);
+}
+
+
+#endif
---
+++ lib/src/Core/KernelHelper.cu
@@ -0,0 +1,44 @@
+#include <Core/KernelHelper.cuh>
+
+__device__
+uint32_t
+ThreadHelper::globalThreadIndex()
+{
+    return blockIdx.x*blockDim.x + threadIdx.x;
+}
+
+__host__ __device__
+Vector2uint32_t
+ThreadHelper::index2pixel(const uint32_t& index, const uint32_t& width, const uint32_t& height)
+{
+    uint32_t y = index/width;
+    uint32_t x = index - width*height;
+    return Vector2uint32_t(x,y);
+}
+
+__host__ __device__
+uint32_t
+ThreadHelper::pixel2index(const Vector2uint32_t& pixel, const uint32_t& width)
+{
+    return pixel.x + pixel.y*width;
+}
+
+
+__host__
+KernelSizeHelper::KernelSize
+KernelSizeHelper::configure(const uint32_t& size)
+{
+    KernelSizeHelper::KernelSize result;
+    
+    result.threads = result.THREADS_PER_BLOCK;
+    
+    
+    result.blocks = size/result.THREADS_PER_BLOCK + 1;
+    
+    if(size % result.THREADS_PER_BLOCK == 0)
+    {
+        ++result.blocks;
+    }
+    
+    return result;
+}
