dc05dd38bef1703b61e88758715ac6aa9ed57821
Domenic Zingsheim <domeniczingsheim@gmail.com>
2021-04-02 00:18:42 +0000

    Add RenderBuffer

---
+++ lib/include/DataStructure/RenderBuffer.cuh
@@ -0,0 +1,8 @@
+#ifndef __CUPBR_DATASTRUCTURE_RENDERBUFFER_CUH
+#define __CUPBR_DATASTRUCTURE_RENDERBUFFER_CUH
+
+#include <DataStructure/Image.cuh>
+
+using RenderBuffer = Image<Vector4uint8_t>;
+
+#endif
