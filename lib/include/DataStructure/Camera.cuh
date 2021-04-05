f46948497c0ba759fd6e12936e4b19c5bf4c7566
Domenic Zingsheim <domeniczingsheim@gmail.com>
2021-04-05 13:58:08 +0000

    Add getter for camera parameters

--- lib/include/DataStructure/Camera.cuh
+++ lib/include/DataStructure/Camera.cuh
@@ -9,11 +9,29 @@ class Camera
     public:
         Camera() = default;
 
+        __host__ __device__
+        Vector3float
+        position() const;
+
+        __host__ __device__
+        Vector3float
+        xAxis() const;
+
+        __host__ __device__
+        Vector3float
+        yAxis() const;
+
+        __host__ __device__
+        Vector3float
+        zAxis() const;
+
     private:
         Vector3float _position = Vector3float(0,0,0);
         Vector3float _xAxis = Vector3float(1,0,0);
         Vector3float _yAxis = Vector3float(0,1,0);
         Vector3float _zAxis = Vector3float(0,0,1);
 };
+
+#include "../../src/DataStructure/CameraDetail.cuh"
 
 #endif
