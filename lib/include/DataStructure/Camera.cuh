6b589e0540536c2440b32a78e7d4c32d571ed237
Domenic Zingsheim <domeniczingsheim@gmail.com>
2021-04-05 14:44:02 +0000

    Camera: Add documentation

--- lib/include/DataStructure/Camera.cuh
+++ lib/include/DataStructure/Camera.cuh
@@ -4,32 +4,54 @@
 #include <Core/CUDA.cuh>
 #include <Math/Vector.h>
 
+/**
+*   @brief A class to model a camera 
+*/
 class Camera
 {
     public:
+        /**
+        *   @brief Default constructor 
+        */
         Camera() = default;
 
+        /**
+        *   @brief Get the world position of the camera 
+        *   @return The camera position in world space
+        */
         __host__ __device__
         Vector3float
         position() const;
 
+        /**
+        *   @brief The x Axis of the camera plane
+        *   @return The vector defining the Image plane into x direction in world space 
+        */ 
         __host__ __device__
         Vector3float
         xAxis() const;
 
+        /**
+        *   @brief The y Axis of the camera plane
+        *   @return The vector defining the Image plane into y direction in world space 
+        */ 
         __host__ __device__
         Vector3float
         yAxis() const;
 
+        /**
+        *   @brief The z Axis to the camera plane
+        *   @return The vector pointing from the camera position to the image plane in world space 
+        */
         __host__ __device__
         Vector3float
         zAxis() const;
 
     private:
-        Vector3float _position = Vector3float(0,0,0);
-        Vector3float _xAxis = Vector3float(1,0,0);
-        Vector3float _yAxis = Vector3float(0,1,0);
-        Vector3float _zAxis = Vector3float(0,0,1);
+        Vector3float _position = Vector3float(0,0,0);   /**< The camera position in world space */
+        Vector3float _xAxis = Vector3float(1,0,0);      /**< The image plane x Axis in world space */
+        Vector3float _yAxis = Vector3float(0,1,0);      /**< The image plane y Axis in world space */
+        Vector3float _zAxis = Vector3float(0,0,1);      /**< The displacement between position and image plane in world space */
 };
 
 #include "../../src/DataStructure/CameraDetail.cuh"
