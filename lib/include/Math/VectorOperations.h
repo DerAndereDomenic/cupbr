4a7f38595dcb119b86793c11f4e930b782300a3b
Domenic <domeniczingsheim@gmail.com>
2021-04-08 17:46:48 +0000

    Add documentation for vector operations

--- lib/include/Math/VectorOperations.h
+++ lib/include/Math/VectorOperations.h
@@ -4,122 +4,290 @@
 #include <Core/CUDA.cuh>
 #include "VectorTypes_fwd.h"
 
+/**
+*   @brief Add two vectors
+*   @param[in] lhs The first vector
+*   @param[in] rhs The second vector
+*   @return The elementwise sum of these two vectors
+*/
 template<typename T>
 __host__ __device__
 Vector2<T> operator+(const Vector2<T>& lhs, const Vector2<T>& rhs);
 
+/**
+*   @brief Subtract two vectors
+*   @param[in] lhs The first vector
+*   @param[in] rhs The second vector
+*   @return The elementwise subtraction of these two vectors
+*/
 template<typename T>
 __host__ __device__
 Vector2<T> operator-(const Vector2<T>& lhs, const Vector2<T>& rhs);
  
+/**
+*   @brief Multiply a vector with a scalar
+*   @param[in] lhs The vector
+*   @param[in] rhs The scalar
+*   @return The elementwise product of the vector with the scalar
+*/
 template<typename T>
 __host__ __device__
 Vector2<T> operator*(const Vector2<T>& lhs, const T& rhs);
 
+/**
+*   @brief Multiply a vector with a scalar
+*   @param[in] lhs The scalar
+*   @param[in] rhs The vector
+*   @return The elementwise product of the vector with the scalar
+*/
 template<typename T>
 __host__ __device__
 Vector2<T> operator*(const T& lhs, const Vector2<T>& rhs);
 
+/**
+*   @brief Multiply two vectors
+*   @param[in] lhs The first vector
+*   @param[in] rhs The second vector
+*   @return Elementwise product between these two vectors
+*/
 template<typename T>
 __host__ __device__
 Vector2<T> operator*(const Vector2<T>& lhs, const Vector2<T>& rhs);
 
+/**
+*   @brief Divide a vector by a scalar
+*   @param[in] lhs The vector
+*   @param[in] rhs The scalar
+*   @return The vector where each component was divided by the scalar
+*/
 template<typename T>
 __host__ __device__
 Vector2<T> operator/(const Vector2<T>& lhs, const T& rhs);
 
+/**
+*   @brief Adds the rhs to the lhs
+*   @param[in] lhs The first vector
+*   @param[in] rhs The second vector
+*/
 template<typename T>
 __host__ __device__
 void operator+=(Vector2<T>& lhs, const Vector2<T>& rhs);
 
+/**
+*   @brief Substracts the rhs from the lhs
+*   @param[in] lhs The first vector
+*   @param[in] rhs The second vector
+*/
 template<typename T>
 __host__ __device__
 void operator-=(Vector2<T>& lhs, const Vector2<T>& rhs);
  
+/**
+*   @brief Multiplies a vector with a scalar
+*   @param[in] lhs The vector
+*   @param[in] rhs The scalar
+*/
 template<typename T>
 __host__ __device__
 void operator*=(Vector2<T> & lhs, const T & rhs);
 
+/**
+*   @brief Divides a vector component wise with a scalar
+*   @param[in] lhs The vector
+*   @param[in] rhs The scalar
+*/
 template<typename T>
 __host__ __device__
 void operator/=(Vector2<T>& lhs, const T& rhs);
 
+/**
+*   @brief Add two vectors
+*   @param[in] lhs The first vector
+*   @param[in] rhs The second vector
+*   @return The elementwise sum of these two vectors
+*/
 template<typename T>
 __host__ __device__
 Vector3<T> operator+(const Vector3<T>& lhs, const Vector3<T>& rhs);
 
+/**
+*   @brief Subtract two vectors
+*   @param[in] lhs The first vector
+*   @param[in] rhs The second vector
+*   @return The elementwise subtraction of these two vectors
+*/
 template<typename T>
 __host__ __device__
 Vector3<T> operator-(const Vector3<T>& lhs, const Vector3<T>& rhs);
 
+/**
+*   @brief Multiply a vector with a scalar
+*   @param[in] lhs The vector
+*   @param[in] rhs The scalar
+*   @return The elementwise product of the vector with the scalar
+*/
 template<typename T>
 __host__ __device__
 Vector3<T> operator*(const Vector3<T>& lhs, const T& rhs);
 
+/**
+*   @brief Multiply a vector with a scalar
+*   @param[in] lhs The scalar
+*   @param[in] rhs The vector
+*   @return The elementwise product of the vector with the scalar
+*/
 template<typename T>
 __host__ __device__
 Vector3<T> operator*(const T& lhs, const Vector3<T>& rhs);
 
+/**
+*   @brief Multiply two vectors
+*   @param[in] lhs The first vector
+*   @param[in] rhs The second vector
+*   @return Elementwise product between these two vectors
+*/
 template<typename T>
 __host__ __device__
 Vector3<T> operator*(const Vector3<T>& lhs, const Vector3<T>& rhs);
 
+/**
+*   @brief Divide a vector by a scalar
+*   @param[in] lhs The vector
+*   @param[in] rhs The scalar
+*   @return The vector where each component was divided by the scalar
+*/
 template<typename T>
 __host__ __device__
 Vector3<T> operator/(const Vector3<T>& lhs, const T& rhs);
 
+/**
+*   @brief Adds the rhs to the lhs
+*   @param[in] lhs The first vector
+*   @param[in] rhs The second vector
+*/
 template<typename T>
 __host__ __device__
 void operator+=(Vector3<T>& lhs, const Vector3<T>& rhs);
 
+/**
+*   @brief Substracts the rhs from the lhs
+*   @param[in] lhs The first vector
+*   @param[in] rhs The second vector
+*/
 template<typename T>
 __host__ __device__
 void operator-=(Vector3<T>& lhs, const Vector3<T>& rhs);
 
+/**
+*   @brief Multiplies a vector with a scalar
+*   @param[in] lhs The vector
+*   @param[in] rhs The scalar
+*/
 template<typename T>
 __host__ __device__
 void operator*=(Vector3<T>& lhs, const T& rhs);
 
+/**
+*   @brief Divides a vector component wise with a scalar
+*   @param[in] lhs The vector
+*   @param[in] rhs The scalar
+*/
 template<typename T>
 __host__ __device__
 void operator/=(Vector3<T>& lhs, const T& rhs);
 
+/**
+*   @brief Add two vectors
+*   @param[in] lhs The first vector
+*   @param[in] rhs The second vector
+*   @return The elementwise sum of these two vectors
+*/
 template<typename T>
 __host__ __device__
 Vector4<T> operator+(const Vector4<T>& lhs, const Vector4<T>& rhs);
 
+/**
+*   @brief Subtract two vectors
+*   @param[in] lhs The first vector
+*   @param[in] rhs The second vector
+*   @return The elementwise subtraction of these two vectors
+*/
 template<typename T>
 __host__ __device__
 Vector4<T> operator-(const Vector4<T>& lhs, const Vector4<T>& rhs);
 
+/**
+*   @brief Multiply a vector with a scalar
+*   @param[in] lhs The vector
+*   @param[in] rhs The scalar
+*   @return The elementwise product of the vector with the scalar
+*/
 template<typename T>
 __host__ __device__
 Vector4<T> operator*(const Vector4<T>& lhs, const T& rhs);
 
+/**
+*   @brief Multiply a vector with a scalar
+*   @param[in] lhs The scalar
+*   @param[in] rhs The vector
+*   @return The elementwise product of the vector with the scalar
+*/
 template<typename T>
 __host__ __device__
 Vector4<T> operator*(const T& lhs, const Vector4<T>& rhs);
 
+/**
+*   @brief Multiply two vectors
+*   @param[in] lhs The first vector
+*   @param[in] rhs The second vector
+*   @return Elementwise product between these two vectors
+*/
 template<typename T>
 __host__ __device__
 Vector4<T> operator*(const Vector4<T>& lhs, const Vector4<T>& rhs);
 
+/**
+*   @brief Divide a vector by a scalar
+*   @param[in] lhs The vector
+*   @param[in] rhs The scalar
+*   @return The vector where each component was divided by the scalar
+*/
 template<typename T>
 __host__ __device__
 Vector4<T> operator/(const Vector4<T>& lhs, const T& rhs);
 
+/**
+*   @brief Adds the rhs to the lhs
+*   @param[in] lhs The first vector
+*   @param[in] rhs The second vector
+*/
 template<typename T>
 __host__ __device__
 void operator+=(Vector4<T>& lhs, const Vector4<T>& rhs);
 
+/**
+*   @brief Substracts the rhs from the lhs
+*   @param[in] lhs The first vector
+*   @param[in] rhs The second vector
+*/
 template<typename T>
 __host__ __device__
 void operator-=(Vector4<T>& lhs, const Vector4<T>& rhs);
 
+/**
+*   @brief Multiplies a vector with a scalar
+*   @param[in] lhs The vector
+*   @param[in] rhs The scalar
+*/
 template<typename T>
 __host__ __device__
 void operator*=(Vector4<T>& lhs, const T& rhs);
 
+/**
+*   @brief Divides a vector component wise with a scalar
+*   @param[in] lhs The vector
+*   @param[in] rhs The scalar
+*/
 template<typename T>
 __host__ __device__
 void operator/=(Vector4<T>& lhs, const T& rhs);
