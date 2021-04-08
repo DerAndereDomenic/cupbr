10fe85a8e1cb1a5e4c9e29ac6bcda9bb1fb1acfb
Domenic <domeniczingsheim@gmail.com>
2021-04-08 12:56:16 +0000

    Add a ray class

---
+++ lib/include/Geometry/Ray.cuh
@@ -0,0 +1,47 @@
+#ifndef __CUPBR_GEOMETRY_RAY_CUH
+#define __CUPBR_GEOMETRY_RAY_CUH
+
+#include <Math/Vector.h>
+
+/**
+*   @brief Class to model a ray
+*/
+class Ray
+{
+    public:
+        /**
+        *   @brief Default constructor
+        */
+        Ray() = default;
+
+        /**
+        *   @brief Create a ray
+        *   @param[in] origin The ray origin
+        *   @param[in] direction The ray direction
+        *   @note The direction will get normalized by the constructor
+        */
+        Ray(const Vector3float& origin, const Vector3float direction);
+
+        /**
+        *   @brief Get a reference to the ray origin
+        *   @return The ray origin
+        */
+        __host__ __device__
+        Vector3float&
+        origin();
+
+        /**
+        *   @brief Get a reference to the ray direction
+        *   @return The ray direction
+        *   @note Normalized
+        */
+        __host__ __device__
+        Vector3float&
+        direction();
+
+    private:
+        Vector3float _origin;       /**< The ray origin */
+        Vector3float _direction;    /**< The normalized ray direction */
+};
+
+#endif
