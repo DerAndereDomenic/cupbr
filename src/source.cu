3c55b59e316c3125252bb2a4f2be6d67fb43079a
Domenic <domeniczingsheim@gmail.com>
2021-04-08 17:18:31 +0000

    Use ray sphere intersection

--- src/source.cu
+++ src/source.cu
@@ -6,6 +6,8 @@
 #include <Core/KernelHelper.cuh>
 #include <DataStructure/Camera.cuh>
 
+#include <Geometry/Sphere.cuh>
+
 __global__ void fillBuffer(RenderBuffer img, const Camera camera)
 {
     uint32_t tid = ThreadHelper::globalThreadIndex();
@@ -24,40 +26,15 @@ __global__ void fillBuffer(RenderBuffer img, const Camera camera)
 
     const Vector3float world_pos = camera.position() + camera.zAxis() + ratio_x*camera.xAxis() + ratio_y*camera.yAxis();
 
-    const Vector3float sphere_pos = Vector3float(0,0,2);
-    const float r = 1.0f;
-
-    const Vector3float origin = camera.position();
-    Vector3float direction = world_pos - origin;
-    Math::normalize(direction);
+    Sphere sphere(Vector3float(0,0,2), 1);
+    Ray ray(camera.position(), world_pos - camera.position());
     
     //Compute intersection
-    Vector3float intersection = Vector3float(INFINITY, INFINITY, INFINITY);
-
-    float a = Math::dot(direction, direction);
-    Vector3float OS = origin - sphere_pos;
-    float b = 2.0f * Math::dot(direction, OS);
-    float c = Math::dot(OS, OS) - r*r;
-    float disc = b*b - 4*a*c;
-
-    if(disc > 0)
-    {
-        float distSqrt = sqrtf(disc);
-        float q = b<0?(-b-distSqrt)/2.0f : (-b+distSqrt)/2.0f;
-        float t0 = q/a;
-        float t1 = c/q;
-        t0 = min(t0,t1);
-        t1 = max(t0,t1);
-        if(t1 >= 0)
-        {
-            float t = t0 < 0 ? t1 : t0;
-            intersection = origin+t*direction;
-        }
-    }
+    Vector4float intersection = sphere.computeRayIntersection(ray);
 
     //"Tone mapping"
 
-    int8_t ratio = intersection.x == INFINITY ? 0 : 255;
+    int8_t ratio = intersection.w == INFINITY ? 0 : 255;
 
     img[tid] = Vector4uint32_t(ratio, ratio, ratio,255);
 }
