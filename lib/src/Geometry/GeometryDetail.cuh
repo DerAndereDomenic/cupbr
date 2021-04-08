b363bded1357177eee4eb9d59edf5b31fd215222
Domenic <domeniczingsheim@gmail.com>
2021-04-08 16:51:28 +0000

    Inline methods

--- lib/src/Geometry/GeometryDetail.cuh
+++ lib/src/Geometry/GeometryDetail.cuh
@@ -2,14 +2,14 @@
 #define __CUPBR_GEOMETRY_GEOMETRYDETAIL_CUH
 
 __host__ __device__
-Vector4float
+inline Vector4float
 Geometry::computeRayIntersection(const Ray& ray)
 {
     return Vector4float(INFINITY);
 }
 
 __host__ __device__
-Vector3float
+inline Vector3float
 Geometry::getNormal(const Vector3float& x)
 {
     return Vector3float(INFINITY);
