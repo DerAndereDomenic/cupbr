60c847bc0c660e214f2f561cb43b824527dbe9da
Domenic <domeniczingsheim@gmail.com>
2021-04-09 18:25:16 +0000

    Add basic rotation

--- lib/src/DataStructure/Camera.cu
+++ lib/src/DataStructure/Camera.cu
@@ -19,4 +19,42 @@ Camera::processInput(GLFWwindow* window)
     {
         _position.z += 0.1f;
     }
+
+    if(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_1) == GLFW_PRESS)
+    {
+        float alpha = 0.01f;
+        float c = cosf(alpha);
+        float s = sinf(alpha);
+
+        float x = c*_zAxis.x + s*_zAxis.z;
+        float y = _zAxis.y;
+        float z = -s*_zAxis.x + c*_zAxis.z;
+
+        _zAxis = Vector3float(x,y,z);
+
+        x = c*_xAxis.x + s*_xAxis.z;
+        y = _xAxis.y;
+        z = -s*_xAxis.x + c*_xAxis.z;
+
+        _xAxis = Vector3float(x,y,z);
+    }
+
+    if(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_2) == GLFW_PRESS)
+    {
+        float alpha = -0.01f;
+        float c = cosf(alpha);
+        float s = sinf(alpha);
+
+        float x = c*_zAxis.x + s*_zAxis.z;
+        float y = _zAxis.y;
+        float z = -s*_zAxis.x + c*_zAxis.z;
+
+        _zAxis = Vector3float(x,y,z);
+
+        x = c*_xAxis.x + s*_xAxis.z;
+        y = _xAxis.y;
+        z = -s*_xAxis.x + c*_xAxis.z;
+
+        _xAxis = Vector3float(x,y,z);
+    }
 }
