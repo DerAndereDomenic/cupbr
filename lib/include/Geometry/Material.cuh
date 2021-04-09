b3516d21eb96a73039cef1534e759b516089a07b
Domenic <domeniczingsheim@gmail.com>
2021-04-09 21:26:29 +0000

    Add different material types

---
+++ lib/include/Geometry/Material.cuh
@@ -0,0 +1,15 @@
+#ifndef __CUPBR_GEOMETRY_MATERIAL_CUH
+#define __CUPBR_GEOMETRY_MATERIAL_CUH
+
+/**
+*   @brief The types of materials supported
+*/
+enum MaterialType
+{
+    LAMBERT,
+    PHONG,
+    MIRROR,
+    GLASS
+};
+
+#endif
