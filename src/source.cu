220674e6a17d6c9299e1ded01da211f06fe8cce0
Domenic <domeniczingsheim@gmail.com>
2021-04-09 20:55:11 +0000

    Add a plane

--- src/source.cu
+++ src/source.cu
@@ -7,6 +7,7 @@
 #include <DataStructure/Camera.cuh>
 
 #include <Geometry/Sphere.cuh>
+#include <Geometry/Plane.cuh>
 
 __global__ void fillBuffer(RenderBuffer img, const Camera camera)
 {
@@ -27,11 +28,15 @@ __global__ void fillBuffer(RenderBuffer img, const Camera camera)
     const Vector3float world_pos = camera.position() + camera.zAxis() + ratio_x*camera.xAxis() + ratio_y*camera.yAxis();
 
     Sphere sphere(Vector3float(0,0,2), 1);
+    Plane plane(Vector3float(0,-1,0), Vector3float(0,1,0));
     Ray ray(camera.position(), world_pos - camera.position());
     
     //Compute intersection
-    Vector4float intersection = sphere.computeRayIntersection(ray);
+    Vector4float intersection_sphere = sphere.computeRayIntersection(ray);
+    Vector4float intersection_plane = plane.computeRayIntersection(ray);
+    Vector4float intersection = intersection_plane.w < intersection_sphere.w ? intersection_plane : intersection_sphere;
     Vector3float intersection_point = Vector3float(intersection);
+    Vector3float normal = intersection_plane.w < intersection_sphere.w ? plane.getNormal(intersection_point) : plane.getNormal(intersection_point);
 
     //Lighting
     const Vector3float lightPos(1,2,-2);
@@ -41,7 +46,7 @@ __global__ void fillBuffer(RenderBuffer img, const Camera camera)
     Vector3float lightDir = Math::normalize(lightPos - intersection_point);
     float d = Math::norm(intersection_point-lightPos);
     Vector3float lightRadiance = lightIntensity/(d*d);
-    float cosTerm = max(0.0f,Math::dot(sphere.getNormal(intersection_point), lightDir));
+    float cosTerm = max(0.0f,Math::dot(normal, lightDir));
     Vector3float radiance = brdf*lightRadiance*cosTerm;
 
     //Tone mapping
