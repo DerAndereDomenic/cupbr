893b92f93bb81894dad823f1259292829623bf2e
Domenic <domeniczingsheim@gmail.com>
2021-04-10 14:32:17 +0000

    Inline functions

--- lib/src/Geometry/MaterialDetail.cuh
+++ lib/src/Geometry/MaterialDetail.cuh
@@ -2,7 +2,7 @@
 #define __CUPBR_GEOMETRY_MATERIALDETAIL_CUH
 
 __host__ __device__
-Vector3float
+inline Vector3float
 Material::brdf(const Vector3float& position, const Vector3float& inc_dir, const Vector3float& out_dir, const Vector3float& normal)
 {
     switch(type)
@@ -33,14 +33,14 @@ Material::brdf(const Vector3float& position, const Vector3float& inc_dir, const 
 }
 
 __host__ __device__
-Vector3float
+inline Vector3float
 Material::brdf_lambert()
 {
     return albedo_d/static_cast<float>(M_PI);
 }
 
 __host__ __device__
-Vector3float
+inline Vector3float
 Material::brdf_phong(const Vector3float& position, const Vector3float& inc_dir, const Vector3float& out_dir, const Vector3float& normal)
 {
     Vector3float halfDir = Math::normalize(inc_dir + out_dir);
--- lib/src/Geometry/RayDetail.cuh
+++ lib/src/Geometry/RayDetail.cuh
@@ -2,6 +2,7 @@
 #define __CUPBR_GEOMETRY_RAYDETAIL_CUH
 
 __host__ __device__
+inline
 Ray::Ray(const Vector3float& origin, const Vector3float direction)
     :_origin(origin),
      _direction(Math::normalize(direction))
@@ -10,14 +11,14 @@ Ray::Ray(const Vector3float& origin, const Vector3float direction)
 }
 
 __host__ __device__
-Vector3float
+inline Vector3float
 Ray::origin() const
 {
     return _origin;
 }
 
 __host__ __device__
-Vector3float
+inline Vector3float
 Ray::direction() const
 {
     return _direction;
