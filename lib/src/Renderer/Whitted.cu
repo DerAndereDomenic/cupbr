934ce0e64825fe4b040bdb0cafc87c5554e66ccb
Domenic <domeniczingsheim@gmail.com>
2021-04-12 21:54:07 +0000

    Add whitted raytracing framework

---
+++ lib/src/Renderer/Whitted.cu
@@ -0,0 +1,11 @@
+#include <Renderer/Whitted.cuh>
+
+void
+PBRendering::whitted(const Scene scene,
+                     const uint32_t& scene_size,
+                     const Camera& camera,
+                     const uint32_t& maxTraceDepth,
+                     Image<Vector3float>* output_img)
+{
+    
+}
