9d70918ac63775389c97ab55a9911a0b97f05802
Domenic <domeniczingsheim@gmail.com>
2021-04-12 21:11:12 +0000

    Add ray tracing module

---
+++ lib/include/Renderer/RayTracer.cuh
@@ -0,0 +1,21 @@
+#ifndef __CUPBR_RENDERER_RAYTRACER_CUH
+#define __CUPBR_RENDERER_RAYTRACER_CUH
+
+#include <Scene/Scene.cuh>
+#include <DataStructure/Image.cuh>
+
+namespace PBRendering
+{
+    /**
+    *   @brief Render the scene using conventional ray tracing
+    *   @param[in] scene The scene to render
+    *   @param[in] scene_size The scene size
+    *   @param[out] output_img The resulting HDR rendering 
+    */
+    void
+    raytracing(const Scene scene,
+               const uint32_t& scene_size,
+               Image<Vector3float>* output_img);
+}
+
+#endif
