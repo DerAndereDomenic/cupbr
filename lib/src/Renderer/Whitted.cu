f87e2dc0f6bb372718f94756f806315bff8e4792
Domenic <domeniczingsheim@gmail.com>
2021-04-12 21:57:48 +0000

    Add and launch kernel

--- lib/src/Renderer/Whitted.cu
+++ lib/src/Renderer/Whitted.cu
@@ -1,5 +1,18 @@
 #include <Renderer/Whitted.cuh>
 
+namespace detail
+{
+    __global__ void
+    whitted_kernel(const Scene scene,
+                   const uint32_t scene_size,
+                   const Camera camera,
+                   const uint32_t maxTraceDepth,
+                   Image<Vector3float> output_img)
+    {
+
+    }
+}
+
 void
 PBRendering::whitted(const Scene scene,
                      const uint32_t& scene_size,
@@ -7,5 +20,11 @@ PBRendering::whitted(const Scene scene,
                      const uint32_t& maxTraceDepth,
                      Image<Vector3float>* output_img)
 {
-    
+    KernelSizeHelper::KernelSize config = KernelSizeHelper::configure(output_img->size());
+    detail::whitted_kernel<<<config.blocks, config.threads>>>(scene,
+                                                              scene_size,
+                                                              camera,
+                                                              maxTraceDepth,
+                                                              *output_img);
+    cudaSafeCall(cudaDeviceSynchronize());
 }
