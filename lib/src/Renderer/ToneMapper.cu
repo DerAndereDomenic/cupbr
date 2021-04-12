a1333b1947f20d6331b0f05d0a524cdc48b7adf4
Domenic <domeniczingsheim@gmail.com>
2021-04-12 18:38:27 +0000

    Implement reinhard tone mapping kernel

--- lib/src/Renderer/ToneMapper.cu
+++ lib/src/Renderer/ToneMapper.cu
@@ -7,6 +7,38 @@
 #include <DataStructure/RenderBuffer.cuh>
 #include <memory>
 
+#include <Core/KernelHelper.cuh>
+#include <Math/Functions.cuh>
+
+namespace detail
+{
+    __global__ void
+    reinhard_kernel(const Image<Vector3float> hdr_image, RenderBuffer output)
+    {
+        const uint32_t tid = ThreadHelper::globalThreadIndex();
+
+        if(tid >= hdr_image.size())
+        {
+            return;
+        }
+
+        Vector3float radiance = hdr_image[tid];
+        Vector3uint8_t color(0);
+
+        float mapped_red = powf(1.0 - expf(-radiance.x), 1.0f/2.2f);
+        float mapped_green = powf(1.0 - expf(-radiance.y), 1.0f/2.2f);
+        float mapped_blue = powf(1.0 - expf(-radiance.z), 1.0f/2.2);
+
+        uint8_t red = static_cast<uint8_t>(Math::clamp(mapped_red, 0.0f, 1.0f)*255.0f);
+        uint8_t green = static_cast<uint8_t>(Math::clamp(mapped_green, 0.0f, 1.0f)*255.0f);
+        uint8_t blue = static_cast<uint8_t>(Math::clamp(mapped_blue, 0.0f, 1.0f)*255.0f);
+
+        color = Vector3uint8_t(red, green, blue);
+
+        output[tid] = Vector4uint8_t(color,255);
+    }
+}
+
 class ToneMapper::Impl()
 {
     public:
