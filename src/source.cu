5b51db619c7d1ff48b35e0a68a9a464df8351b50
Domenic <domeniczingsheim@gmail.com>
2021-04-12 21:26:04 +0000

    Call renderer method

--- src/source.cu
+++ src/source.cu
@@ -10,56 +10,10 @@
 
 #include <DataStructure/Camera.cuh>
 
-#include <Geometry/Sphere.cuh>
-#include <Geometry/Plane.cuh>
-
 #include <Scene/SceneLoader.cuh>
 
 #include <Renderer/ToneMapper.cuh>
 #include <Renderer/PBRenderer.cuh>
-
-__global__ void fillBuffer(Image<Vector3float> img, const Scene scene, const uint32_t scene_size, const Camera camera)
-{
-    uint32_t tid = ThreadHelper::globalThreadIndex();
-
-    if(tid >= img.size())
-    {
-        return;
-    }
-
-    Ray ray = Tracing::launchRay(tid, img.width(), img.height(), camera);
-
-    //Scene
-    const Vector3float lightPos(0.0f,0.9f,2.0f);
-
-    LocalGeometry geom = Tracing::traceRay(scene, scene_size, ray);
-
-    Vector3float inc_dir = Math::normalize(camera.position() - geom.P);
-    Vector3float lightDir = Math::normalize(lightPos - geom.P);
-
-
-    //Lighting
-
-    Vector3float brdf = geom.material.brdf(geom.P, inc_dir, lightDir, geom.N);
-    Vector3float lightIntensity = Vector3float(10,10,10); //White light
-    float d = Math::norm(geom.P-lightPos);
-    Vector3float lightRadiance = lightIntensity/(d*d);
-    float cosTerm = max(0.0f,Math::dot(geom.N, lightDir));
-    Vector3float radiance = brdf*lightRadiance*cosTerm;
-
-    //Shadow
-    if(geom.depth != INFINITY)
-    {
-        Ray shadow_ray(geom.P-EPSILON*ray.direction(), lightDir);
-        
-        if(!Tracing::traceVisibility(scene, scene_size, d, shadow_ray))
-        {
-            radiance = 0;
-        }
-    }
-
-    img[tid] = radiance;
-}
 
 int main()
 {
@@ -119,8 +73,7 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
 
         std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-        fillBuffer<<<config.blocks, config.threads>>>(*pbrenderer.getOutputImage(),scene,scene_size,camera);
-        cudaSafeCall(cudaDeviceSynchronize());
+        pbrenderer.render(camera);
 
         mapper->toneMap();
         renderer.renderTexture(mapper->getRenderBuffer());
