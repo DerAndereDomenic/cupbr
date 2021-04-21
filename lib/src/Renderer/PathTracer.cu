9ff283d7b3b98454dc1e55b19e2775b877b0fb88
Domenic <domeniczingsheim@gmail.com>
2021-04-21 17:36:31 +0000

    Add and launch kernel

--- lib/src/Renderer/PathTracer.cu
+++ lib/src/Renderer/PathTracer.cu
@@ -1,6 +1,26 @@
 #include <Renderer/PathTracer.cuh>
+#include <Core/KernelHelper.cuh>
+#include <Core/Tracing.cuh>
+#include <Geometry/Sphere.cuh>
+#include <Geometry/Plane.cuh>
 
+namespace detail
+{
+    __global__ void
+    pathtracer_kernel(const Scene scene,
+                      const uint32_t scene_size,
+                      const Camera camera,
+                      const uint32_t maxTraceDepth,
+                      Image<Vector3float> img)
+    {
+        const uint32_t tid = ThreadHelper::globalThreadIndex();
 
+        if(tid >= img.size())
+        {
+            return;
+        }
+    }
+}
 
 void
 PBRendering::pathtracing(const Scene scene,
@@ -9,5 +29,11 @@ PBRendering::pathtracing(const Scene scene,
                          const uint32_t& maxTraceDepth,
                          Image<Vector3float>* output_img)
 {
-
+    const KernelSizeHelper::KernelSize config = KernelSizeHelper::configure(output_img->size());
+    detail::pathtracer_kernel<<<config.blocks, config.threads>>>(scene, 
+                                                                 scene_size, 
+                                                                 camera, 
+                                                                 maxTraceDepth, 
+                                                                 *output_img);
+    cudaSafeCall(cudaDeviceSynchronize());
 }
