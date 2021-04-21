c948a489f35369d190f2fd52c165acad8e474eb5
Domenic <domeniczingsheim@gmail.com>
2021-04-21 17:29:50 +0000

    Add path tracer class

---
+++ lib/src/Renderer/PathTracer.cu
@@ -0,0 +1,13 @@
+#include <Renderer/PathTracer.cuh>
+
+
+
+void
+PBRendering::pathtracing(const Scene scene,
+                         const uint32_t& scene_size,
+                         const Camera& camera,
+                         const uint32_t& maxTraceDepth,
+                         Image<Vector3float>* output_img)
+{
+
+}
