98071206a032374a674e6869819ff7b90e064a15
Domenic <domeniczingsheim@gmail.com>
2021-05-12 00:35:46 +0200

    Compute ray triangle intersection

--- lib/src/Geometry/TriangleDetail.cuh
+++ lib/src/Geometry/TriangleDetail.cuh
@@ -19,18 +19,25 @@ __host__ __device__
 inline Vector4float
 Triangle::computeRayIntersection(const Ray& ray)
 {
-    float PdotN = Math::dot(_vertex1, _normal);
-    float OdotN = Math::dot(ray.origin(), _normal);
-    float DdotN = Math::dot(ray.direction(), _normal);
+    Vector3float v0v1 = _vertex2 - _vertex1;
+    Vector3float v0v2 = _vertex3 - _vertex1;
+    Vector3float pvec = Math::cross(ray.direction(), v0v2);
+    float det = Math::dot(v0v1, pvec);
 
-    if(Math::safeFloatEqual(DdotN, 0.0f))
-    {
-        return Vector4float(INFINITY);
-    }
+    if(Math::safeFloatEqual(det, 0.0f))return Vector4float(INFINITY);
 
-    float t = (PdotN - OdotN)/DdotN;
+    float invDet = 1.0f / det;
 
-    if(t < 0)return Vector4float(INFINITY);
+    Vector3float tvec = ray.origin() - _vertex1;
+
+    float u = Math::dot(tvec, pvec) * invDet;
+    if(u < 0.0f || u > 1.0f) return Vector4float(INFINITY);
+
+    Vector3float qvec = Math::dot(tvec, v0v1);
+    float v = Math::dot(ray.direction(), qvec) * invDet;
+    if(v < 0.0f || u + v > 1.0f) return Vector4float(INFINITY);
+
+    float t = Math::dot(v0v2, qvec) * invDet;
 
     return Vector4float(ray.origin() + t*ray.direction(), t);
 }
