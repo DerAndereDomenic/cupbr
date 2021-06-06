4d9fb6b56202c25c78fd57c19a72b4c68608c001
Domenic <domeniczingsheim@gmail.com>
2021-06-07 01:53:39 +0200

    Add register method and constructor

--- lib/src/Interaction/Interactor.cu
+++ lib/src/Interaction/Interactor.cu
@@ -2,6 +2,30 @@
 
 class Interactor::Impl
 {
+    public:
+        Impl() = default;
+        
+        ~Impl() = default;
 
+        GLFWwindow* window;
+
+        bool window_registered = false;
 };
 
+Interactor::Interactor()
+{
+    impl = std::make_unique<Impl>();
+}
+
+void
+Interactor::registerWindow(GLFWwindow* window)
+{
+    impl->window = window;
+    impl->window_registered = true;
+}
+
+void
+Interactor::updateInteraction()
+{
+
+}
