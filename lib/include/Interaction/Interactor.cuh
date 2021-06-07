6f52e28b395a857d8944a3dfab922909b03a9834
Domenic <domeniczingsheim@gmail.com>
2021-06-07 03:04:28 +0200

    Add Docs

--- lib/include/Interaction/Interactor.cuh
+++ lib/include/Interaction/Interactor.cuh
@@ -9,22 +9,46 @@
 #include <DataStructure/Camera.cuh>
 #include <Scene/Scene.cuh>
 
+/**
+*   @brief A class to handle user interactions 
+*/
 class Interactor
 {
     public:
+        /**
+        *   @brief Default constructor 
+        */
         Interactor();
 
+        /**
+        *   @brief Default destructor 
+        */
         ~Interactor();
 
+        /**
+        *   @brief Add the window for which the input should be handled 
+        *   @param[in] window The window
+        */
         void
         registerWindow(GLFWwindow* window);
 
+        /**
+        *   @brief Add the scene we want to interact with 
+        *   @param[in] scene The scene
+        */
         void
         registerScene(Scene& scene);
 
+        /** 
+        *   @brief Add the camera
+        *   @param[in] camera The camera
+        */
         void
         registerCamera(const Camera& camera);
 
+        /**
+        *   @brief This handles the user interaction. It should be called every frame
+        */
         void
         handleInteraction();
     private:
--- lib/include/Interaction/MousePicker.cuh
+++ lib/include/Interaction/MousePicker.cuh
@@ -8,6 +8,16 @@
 
 namespace Interaction
 {
+    /**
+    *   @brief Reads the material of the currently selected pixel
+    *   @param[in] x x-position of the mouse
+    *   @param[in] y y-position of the mouse
+    *   @param[in] width The window width
+    *   @param[in] height The window height
+    *   @param[in] scene The Scene
+    *   @param[in] camera The camera
+    *   @param[out] outMaterial The material found at the selected pixel 
+    */
     void pickMouse(const uint32_t& x,
                    const uint32_t& y,
                    const uint32_t& width,
