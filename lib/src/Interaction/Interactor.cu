9e0d9cef19181d932bfe7622609b2b7cd3e31c23
Domenic <domeniczingsheim@gmail.com>
2021-06-07 02:10:48 +0200

    Implement framework for mouse picking

--- lib/src/Interaction/Interactor.cu
+++ lib/src/Interaction/Interactor.cu
@@ -8,8 +8,11 @@ class Interactor::Impl
         ~Impl() = default;
 
         GLFWwindow* window;
+        int32_t width;
+        int32_t height;
 
         bool window_registered = false;
+        bool pressed = false;
 };
 
 Interactor::~Interactor() = default;
@@ -23,11 +26,33 @@ void
 Interactor::registerWindow(GLFWwindow* window)
 {
     impl->window = window;
+    glfwGetWindowSize(impl->window, &(impl->width), &(impl->height));
+
     impl->window_registered = true;
 }
 
 void
 Interactor::handleInteraction()
 {
+    int32_t state = glfwGetMouseButton(impl->window, GLFW_MOUSE_BUTTON_LEFT);
+    if(state == GLFW_PRESS && !(impl->pressed))
+    {
+        impl->pressed = true;
+        double xpos, ypos;
+        glfwGetCursorPos(impl->window, &xpos, &ypos);
 
+        int32_t x = static_cast<int32_t>(xpos);
+        int32_t y = static_cast<int32_t>(ypos);
+
+        if(x >= 0 && x < impl->width && y >= 0 && y < impl->height)
+        {
+            //do mouse picking
+        }
+    }
+
+    if(state == GLFW_RELEASE && impl->pressed)
+    {
+        impl->pressed = false;
+    }
+    
 }
