ca9fd6d3d630eb4513ed6a0dae17c74897790dac
Domenic <domeniczingsheim@gmail.com>
2021-06-07 02:22:56 +0200

    Add parameter list to mouse picker

--- lib/include/Interaction/MousePicker.cuh
+++ lib/include/Interaction/MousePicker.cuh
@@ -1,9 +1,18 @@
 #ifndef __CUPBR_INTERACTION_MOUSEPICKER_CUH
 #define __CUPBR_INTERACTION_MOUSEPICKER_CUH
 
+#include <cstdint>
+#include <Scene/Scene.cuh>
+#include <DataStructure/Camera.cuh>
+#include <Geometry/Geometry.cuh>
+
 namespace Interaction
 {
-    void pickMouse();
+    void pickMouse(const uint32_t x,
+                   const uint32_t y,
+                   Scene& scene,
+                   Camera& camera,
+                   Geometry* outGeometry);
 }
 
 #endif
--- lib/src/Interaction/MousePicker.cu
+++ lib/src/Interaction/MousePicker.cu
@@ -1,1 +1,11 @@
 #include <Interaction/MousePicker.cuh>
+
+void 
+Interaction::pickMouse(const uint32_t x,
+                       const uint32_t y,
+                       Scene& scene,
+                       Camera& camera,
+                       Geometry* outGeometry)
+{
+
+}
