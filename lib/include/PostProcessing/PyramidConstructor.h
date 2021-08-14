3675c2487678a8385e3c714f9f902a93a611db85
Domenic Zingsheim <domeniczingsheim@gmail.com>
2021-08-14 23:47:40 +0200

    Add downsampling of pyramid

--- lib/include/PostProcessing/PyramidConstructor.h
+++ lib/include/PostProcessing/PyramidConstructor.h
@@ -12,6 +12,15 @@ namespace PostProcessing
 	*/
 	void
 	radiance_threshold(Image<Vector3float>* img, const float& threshold);
+
+	/**
+	*	@brief Construct the image pyramid
+	*	@param[in] pyramid (device)
+	*	@param[in] host_pyramid (host)
+	*	@param[in] pyramid_depth The depth of the pyramid
+	*/
+	void
+	construct_pyramid(Image<Vector3float>* pyramid, Image<Vector3float>* host_pyramid, const uint32_t& pyramid_depth);
 }
 
 #endif
--- lib/src/PostProcessing/PyramidConstructor.cu
+++ lib/src/PostProcessing/PyramidConstructor.cu
@@ -21,6 +21,41 @@ namespace detail
 
 		img[tid] = Vector3float(red, green, blue);
 	}
+
+	__global__ void
+	downsample_kernel(Image<Vector3float>* pyramid, const uint32_t depth)
+	{
+		const uint32_t tid = ThreadHelper::globalThreadIndex();
+
+		Image<Vector3float> current_image = pyramid[depth];
+
+		if(tid >= current_image.size())
+		{
+			return;
+		}
+
+		Image<Vector3float> last_image = pyramid[depth - 1];
+		Vector2uint32_t pixel = ThreadHelper::index2pixel(tid, current_image.width(), current_image.height());
+		Vector2uint32_t upsampled = 2u * pixel;
+
+		Vector3float result = 0;
+		Vector2uint32_t sample;
+
+		#define READ_VALUE(u, v)\
+			sample = upsampled + Vector2uint32_t(u,v);\
+			if(sample.x >= last_image.width())sample.x  = last_image.width()-1;\
+			if(sample.y >= last_image.height())sample.y = last_image.height()-1;\
+			result += last_image(sample);
+
+		READ_VALUE(0, 0);
+		READ_VALUE(0, 1);
+		READ_VALUE(1, 0);
+		READ_VALUE(1, 1);
+
+		#undef READ_VALUE
+			
+		current_image[tid] = result / 4.0f;
+	}
 }
 
 void
@@ -30,4 +65,16 @@ PostProcessing::radiance_threshold(Image<Vector3float>* img, const float& thresh
 
 	detail::thresholding_kernel << <config.blocks, config.threads >> > (*img, threshold);
 	cudaDeviceSynchronize();
+}
+
+void
+PostProcessing::construct_pyramid(Image<Vector3float>* pyramid, Image<Vector3float>* host_pyramid, const uint32_t& pyramid_depth)
+{
+	for(uint32_t i = 1; i < pyramid_depth; ++i)
+	{
+		const KernelSizeHelper::KernelSize config = KernelSizeHelper::configure(host_pyramid[i].size());
+
+		detail::downsample_kernel << <config.blocks, config.threads >> > (pyramid, i);
+		cudaDeviceSynchronize();
+	}
 }
