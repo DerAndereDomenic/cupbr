68946552cfe6f05db6d6cbfaaedc34a8891ce240
Domenic Zingsheim <domeniczingsheim@gmail.com>
2021-08-21 22:38:35 +0200

    Reset GDPT

--- lib/src/Renderer/GradientDomain.cu
+++ lib/src/Renderer/GradientDomain.cu
@@ -6,118 +6,122 @@
 
 namespace detail
 {
-    __device__ Vector3float
-    traceImage( const Vector2uint32_t& pixel,
-                const uint32_t& tid,
-                uint32_t& seed,
-                Scene& scene,
-                const Camera& camera,
-                const uint32_t& frameIndex,
-                const uint32_t& maxTraceDepth,
-                Image<Vector3float> img)
+    struct RadiancePayload
     {
-        Ray ray = Tracing::launchRay(pixel, img.width(), img.height(), camera, true, &seed);
-
-        uint32_t trace_depth = 0;
+        uint32_t seed;
         Vector3float radiance = 0;
         Vector3float rayweight = 1;
-        Vector3float inc_dir, lightDir, lightRadiance;
-        float d;
+        Vector3float out_dir;
+        bool next_ray_valid;
+    };
+
+    inline __device__
+    void emissiveIllumintationGDPT(Ray& ray, LocalGeometry& geom)
+    {
+        RadiancePayload* payload = ray.payload<RadiancePayload>();
+
+        payload->radiance += payload->rayweight * geom.material.albedo_e;
+    }
+
+    inline __device__ 
+    void directIlluminationGDPT(Scene& scene, Ray& ray, LocalGeometry& geom, Vector3float& inc_dir)
+    {
+        //Direct illumination
+        RadiancePayload* payload = ray.payload<RadiancePayload>();
+
+        Vector3float normal = geom.N;
+
+        //Don't shade back facing geometry
+        if(geom.material.type != GLASS && Math::dot(normal, inc_dir) <= 0.0f)
+        {
+            payload->rayweight = 0;
+            return;
+        }
+
+        uint32_t useEnvironmentMap = scene.useEnvironmentMap ? 1 : 0;
+        uint32_t light_sample = static_cast<uint32_t>(Math::rnd(payload->seed) * (scene.light_count + useEnvironmentMap));
 
         Light light;
-
-        do
+        Vector3float lightDir, lightRadiance;
+        float d;
+        if(light_sample != scene.light_count)
         {
-            //Direct illumination
-            LocalGeometry geom = Tracing::traceRay(scene, ray);
-            if(geom.depth == INFINITY)
+            light = *(scene.lights[light_sample]); 
+
+            switch(light.type)
             {
-                if(scene.useEnvironmentMap)
+                case LightType::POINT:
                 {
-                    Vector2uint32_t pixel = Tracing::direction2UV(ray.direction(), scene.environment.width(), scene.environment.height());
-                    radiance += rayweight * scene.environment(pixel);
+                    lightDir = Math::normalize(light.position - geom.P);
+                    d = Math::norm(light.position - geom.P);
+                    lightRadiance = light.intensity / (d*d);
+                }
+                break;
+                case LightType::AREA:
+                {
+                    float xi1 = Math::rnd(payload->seed) * 2.0f - 1.0f;
+                    float xi2 = Math::rnd(payload->seed) * 2.0f - 1.0f;
+
+                    Vector3float sample = light.position + xi1 * light.halfExtend1 + xi2 * light.halfExtend2;
+                    Vector3float n = Math::normalize(Math::cross(light.halfExtend1, light.halfExtend2));
+                    float area = 4.0f*Math::norm(light.halfExtend1) * Math::norm(light.halfExtend2);
+
+                    lightDir = Math::normalize(sample - geom.P);
+                    d = Math::norm(sample - geom.P);
+
+                    float NdotL = Math::dot(lightDir, n);
+                    if(NdotL < 0) NdotL *= -1.0f;
+
+                    float solidAngle =  area * NdotL / (d*d);
+
+                    lightRadiance = light.radiance * solidAngle;
                 }
                 break;
             }
+        }
+        else // Use environment map
+        {
+            Vector4float sample = geom.material.sampleDirection(payload->seed, inc_dir, geom.N);
+            lightDir = Vector3float(sample);
+            d = INFINITY; //TODO: Better way to do this
+            Vector2uint32_t pixel = Tracing::direction2UV(lightDir, scene.environment.width(), scene.environment.height());
+            lightRadiance = scene.environment(pixel)/sample.w;
+        }
+            
+        Ray shadow_ray = Ray(geom.P + 0.001f*lightDir, lightDir);
 
-            Vector3float normal = geom.N;
+        if(Tracing::traceVisibility(scene, d, shadow_ray))
+        {
+            payload->radiance += (scene.light_count+useEnvironmentMap) *
+                                                        fmaxf(0.0f, Math::dot(normal,lightDir)) *
+                                                        geom.material.brdf(geom.P,inc_dir,lightDir,normal) *
+                                                        lightRadiance *
+                                                        payload->rayweight;
+        }
+    }
 
-            inc_dir = Math::normalize(ray.origin() - geom.P);
-
-            uint32_t useEnvironmentMap = scene.useEnvironmentMap ? 1 : 0;
-            uint32_t light_sample = static_cast<uint32_t>(Math::rnd(seed) * (scene.light_count + useEnvironmentMap));
-
-            if(light_sample != scene.light_count)
-            {
-                light = *(scene.lights[light_sample]); 
-
-                switch(light.type)
-                {
-                    case LightType::POINT:
-                    {
-                        lightDir = Math::normalize(light.position - geom.P);
-                        d = Math::norm(light.position - geom.P);
-                        lightRadiance = light.intensity / (d*d);
-                    }
-                    break;
-                    case LightType::AREA:
-                    {
-                        float xi1 = Math::rnd(seed) * 2.0f - 1.0f;
-                        float xi2 = Math::rnd(seed) * 2.0f - 1.0f;
-
-                        Vector3float sample = light.position + xi1 * light.halfExtend1 + xi2 * light.halfExtend2;
-                        Vector3float n = Math::normalize(Math::cross(light.halfExtend1, light.halfExtend2));
-                        float area = 4.0f*Math::norm(light.halfExtend1) * Math::norm(light.halfExtend2);
-
-                        lightDir = Math::normalize(sample - geom.P);
-                        d = Math::norm(sample - geom.P);
-
-                        float NdotL = Math::dot(lightDir, n);
-                        if(NdotL < 0) NdotL *= -1.0f;
-
-                        float solidAngle =  area * NdotL / (d*d);
-
-                        lightRadiance = light.radiance * solidAngle;
-                    }
-                    break;
-                }
-            }
-            else // Use environment map
-            {
-                Vector4float sample = geom.material.sampleDirection(seed, inc_dir, geom.N);
-                lightDir = Vector3float(sample);
-                d = INFINITY; //TODO: Better way to do this
-                Vector2uint32_t pixel = Tracing::direction2UV(lightDir, scene.environment.width(), scene.environment.height());
-                lightRadiance = scene.environment(pixel)/sample.w;
-            }
-                
-            Ray shadow_ray = Ray(geom.P + 0.01f*lightDir, lightDir);
-
-            if(Tracing::traceVisibility(scene, d, shadow_ray))
-            {
-                radiance += (scene.light_count+useEnvironmentMap)*fmaxf(0.0f, Math::dot(normal,lightDir))*geom.material.brdf(geom.P,inc_dir,lightDir,normal)*lightRadiance*rayweight;
-            }
-
-            //Indirect illumination
-            Vector4float direction_p = geom.material.sampleDirection(seed, inc_dir, geom.N);
-            Vector3float direction = Vector3float(direction_p);
-            rayweight = rayweight * fabs(Math::dot(direction, normal))*geom.material.brdf(geom.P, inc_dir, direction, normal)/direction_p.w;
-                 
-            ray = Ray(geom.P+0.01f*direction, direction);
-            ++trace_depth;
-        }while(trace_depth < maxTraceDepth);
-
-        return radiance;
+    inline __device__ 
+    void indirectIlluminationGDPT(Ray& ray, LocalGeometry& geom, Vector3float& inc_dir)
+    {
+        //Indirect illumination
+        RadiancePayload* payload = ray.payload<RadiancePayload>();
+        Vector4float direction_p = geom.material.sampleDirection(payload->seed, inc_dir, geom.N);
+        Vector3float direction = Vector3float(direction_p);
+        if (Math::norm(direction) == 0)
+            return;
+        ray.payload<RadiancePayload>()->rayweight = ray.payload<RadiancePayload>()->rayweight * 
+                                                    fabs(Math::dot(direction, geom.N)) * 
+                                                    geom.material.brdf(geom.P, inc_dir, direction, geom.N)/direction_p.w;
+        payload->out_dir = direction;
+        payload->next_ray_valid = true;
     }
 
     __global__ void
-    gradientdomain_kernel(Scene scene,
+    gdpathtracer_kernel_nee(Scene scene,
                           const Camera camera,
                           const uint32_t frameIndex,
                           const uint32_t maxTraceDepth,
-                          Image<Vector3float> img,
-                          Image<Vector3float> gradient_x,
-                          Image<Vector3float> gradient_y)
+                          Image<Vector3float> img)
     {
         const uint32_t tid = ThreadHelper::globalThreadIndex();
 
@@ -126,89 +130,50 @@ namespace detail
             return;
         }
 
-        Vector2uint32_t pixel = ThreadHelper::index2pixel(tid, img.width(), img.height());
+        uint32_t seed = Math::tea<4>(tid, frameIndex);
 
-        if(pixel.x > 0 && pixel.x < img.width() - 1 && pixel.y > 0 && pixel.y < img.height() - 1)
+        Ray ray = Tracing::launchRay(tid, img.width(), img.height(), camera, true, &seed);
+        RadiancePayload payload;
+        payload.seed = seed;
+        ray.setPayload(&payload);
+
+        uint32_t trace_depth = 0;
+        Vector3float inc_dir;
+
+        Light light;
+
+        do
         {
-            uint32_t seed = Math::tea<4>(tid, frameIndex);
-            Vector3float base = traceImage(pixel,
-                                           tid,
-                                           seed,
-                                           scene,
-                                           camera,
-                                           frameIndex,
-                                           maxTraceDepth,
-                                           img);
-
-            seed = Math::tea<4>(tid, frameIndex);
-
-            Vector3float shiftX = traceImage(pixel + Vector2uint32_t(1,0),
-                                             tid,
-                                             seed,
-                                             scene,
-                                             camera,
-                                             frameIndex,
-                                             maxTraceDepth,
-                                             gradient_x);
-
-            seed = Math::tea<4>(tid, frameIndex);
-
-            Vector3float shiftY = traceImage(pixel + Vector2uint32_t(0,1),
-                                             tid,
-                                             seed,
-                                             scene,
-                                             camera,
-                                             frameIndex,
-                                             maxTraceDepth,
-                                             gradient_y);
-
-            Vector3float gradX = 0.5f*(shiftX - base); 
-            Vector3float gradY = 0.5f*(shiftY - base);
-
-            if(frameIndex > 0)
+            payload.next_ray_valid = false;
+            LocalGeometry geom = Tracing::traceRay(scene, ray);
+            if(geom.depth == INFINITY)
             {
-                const float a = 1.0f/(static_cast<float>(frameIndex) + 1.0f);
-                base = (1.0f-a)*img[tid] + a*base;
-                gradX = (1.0f-a)*gradient_x[tid] + a*gradX;
-                gradY = (1.0f-a)*gradient_y[tid] + a*gradY;
+                if(scene.useEnvironmentMap)
+                {
+                    Vector2uint32_t pixel = Tracing::direction2UV(ray.direction(), scene.environment.width(), scene.environment.height());
+                    payload.radiance += payload.rayweight * scene.environment(pixel);
+                }
+                break;
             }
+            Vector3float inc_dir = Math::normalize(ray.origin() - geom.P);
 
-            img[tid] = base;
-            gradient_x[tid] = gradX;
-            gradient_y[tid] = gradY;
-        }
-    }
+            emissiveIllumintationGDPT(ray, geom);
+            directIlluminationGDPT(scene, ray, geom, inc_dir);
+            indirectIlluminationGDPT(ray, geom, inc_dir);
+                 
+            ray.traceNew(geom.P+0.01f*payload.out_dir, payload.out_dir);
 
-    __global__ void
-    reconstruction_kernel(Image<Vector3float> base,
-                          Image<Vector3float> gradient_x,
-                          Image<Vector3float> gradient_y,
-                          Image<Vector3float> temp)
-    {
-        const uint32_t tid = ThreadHelper::globalThreadIndex();
+            if (!payload.next_ray_valid)break;
+            ++trace_depth;
+        }while(trace_depth < maxTraceDepth);
 
-        if(tid >= base.size())
+        if(frameIndex > 0)
         {
-            return;
+            const float a = 1.0f/(static_cast<float>(frameIndex) + 1.0f);
+            ray.payload<RadiancePayload>()->radiance = (1.0f-a)*img[tid] + a*ray.payload<RadiancePayload>()->radiance;
         }
 
-        Vector2uint32_t pixel = ThreadHelper::index2pixel(tid, base.width(), base.height());
-        
-        if(pixel.x > 0 && pixel.x < base.width() - 1 && pixel.y > 0 && pixel.y < base.height() - 1)
-        {
-            Vector2uint32_t left(pixel.x-1, pixel.y);
-            Vector2uint32_t right(pixel.x+1, pixel.y);
-            Vector2uint32_t up(pixel.x, pixel.y-1);
-            Vector2uint32_t down(pixel.x, pixel.y+1);
-
-            Vector3float v = base(pixel);
-            v += (base(left) + gradient_x(left));
-            v += (base(up) + gradient_y(up));
-            v += (base(right) - gradient_x(pixel));
-            v += (base(down) - gradient_y(pixel));
-
-            temp[tid] = v/5.0f;
-        }
+        img[tid] = ray.payload<RadiancePayload>()->radiance;
     }
 }
 
@@ -224,26 +189,10 @@ PBRendering::gradientdomain(Scene& scene,
                             Image<Vector3float>* output_img)
 {
     const KernelSizeHelper::KernelSize config = KernelSizeHelper::configure(output_img->size());
-    detail::gradientdomain_kernel<<<config.blocks, config.threads>>>(scene, 
-                                                                 camera,
-                                                                 frameIndex,
-                                                                 maxTraceDepth, 
-                                                                 *base,
-                                                                 *gradient_x,
-                                                                 *gradient_y);
+    detail::gdpathtracer_kernel_nee<<<config.blocks, config.threads>>>(scene, 
+                                                                       camera,
+                                                                       frameIndex,
+                                                                       maxTraceDepth,
+                                                                       *output_img);
     cudaSafeCall(cudaDeviceSynchronize());
-
-    //Reconstruct
-    base->copyDevice2DeviceObject(*output_img);
-
-    for(uint32_t i = 0; i < 5; ++i)
-    {
-        detail::reconstruction_kernel<<<config.blocks, config.threads>>>(*output_img,
-                                                                         *gradient_x,
-                                                                         *gradient_y,
-                                                                         *temp);
-        cudaSafeCall(cudaDeviceSynchronize());
-
-        temp->copyDevice2DeviceObject(*output_img);
-    }
 }
