804605a0fddc1b5b116e6fe300f642e94951f42d
Domenic Zingsheim <domeniczingsheim@gmail.com>
2022-03-07 16:31:52 +0100

    Generate training data for PPT CVAE

--- src/SphereTracingGenerator/source.cu
+++ src/SphereTracingGenerator/source.cu
@@ -4,9 +4,11 @@
 #include <CUPBR.h>
 #include <CUNET.h>
 
+#include <Models/LenGen.h>
+
 using namespace cupbr;
 
-struct MediumSettings
+/*struct MediumSettings
 {
     float sigma;
     float phi;
@@ -197,7 +199,7 @@ __global__ void generateSamples(const uint32_t N, FileLine* output)
 
 void generateDatasetForTrainingCVAE()
 {
-    const uint32_t N = 1 << 10;
+    const uint32_t N = 1 << 22;
     printf("Generatirng %i samples\n", N);
 
     FileLine* dev_output = Memory::createDeviceArray<FileLine>(N);
@@ -237,11 +239,60 @@ void generateDatasetForTrainingCVAE()
     Memory::destroyDeviceObject<FileLine>(dev_output);
 }
 
+__global__ void test(const uint32_t N, cunet::LenGen lenGen, float* buffer)
+{
+    const uint32_t tid = ThreadHelper::globalThreadIndex();
+
+    if (tid >= N)
+        return;
+
+    uint32_t seed = Math::tea<4>(tid, 0);
+
+    Vector2float lenLatent = Math::sampleStdNormal2D(seed);
+    float lenInput[4] = {10, 0.9, lenLatent.x, lenLatent.y};
+    float lenOutput[2];
+    lenGen(lenInput, lenOutput);
+
+    float logN = fmaxf(0.0f, lenOutput[0] + Math::sampleStdNormal1D(seed) * expf(0.5*Math::clamp(lenOutput[1], -16.0f, 16.0f)));
+    float n = roundf(expf(logN) + 0.49f);
+
+    buffer[tid] = n;
+}
+
 int run()
 {
     cudaSafeCall(cudaSetDevice(0));
 
-    generateDatasetForTrainingCVAE();
+    uint32_t N = 1 << 22;
+
+    cunet::LenGen lenGen_host;
+    cunet::LenGen* lenGen = cunet::Memory::createDeviceObject<cunet::LenGen>();
+
+    cunet::Memory::copyHost2DeviceObject<cunet::LenGen>(&lenGen_host, lenGen);
+
+    float* buffer = Memory::createDeviceArray<float>(N);
+
+    KernelSizeHelper::KernelSize config = KernelSizeHelper::configure(N);
+    test << <config.blocks, config.threads >> > (N, lenGen_host, buffer);
+    cudaDeviceSynchronize();
+
+    float* host_buffer = Memory::createHostArray<float>(N);
+    Memory::copyDevice2HostArray<float>(N, buffer, host_buffer);
+    Memory::destroyDeviceArray<float>(buffer);
+
+    std::ofstream file;
+    file.open("Evaluation.ds");
+
+    for(uint32_t i = 0; i < N; ++i)
+    {
+        file << host_buffer[i] << "\n";
+    }
+
+    file.close();
+
+
+    Memory::destroyHostArray<float>(host_buffer);
+    //generateDatasetForTrainingCVAE();
 
     return 0;
 }
@@ -250,15 +301,38 @@ int main()
 {
     int exit = run();
     return exit;
-}
+}*/
 
-/*struct PathSummary
+struct PathSummary
 {
+    Vector3float inc_pos;
     Vector3float inc_dir;
     Vector3float out_pos;
     Vector3float out_dir;
     uint32_t num_scattering;
 };
+
+struct DataPoint
+{
+    float theta_dir_in;
+    uint32_t num_scattering;
+    float theta_pos_out;
+    float phi_pos_out;
+    float theta_dir_out;
+    float phi_dir_out;
+};
+
+__device__ float distanceToBoundary(const Vector3float& x, const Vector3float& d)
+{
+    float b = 2.0f * Math::dot(x,d);
+    float c = Math::dot(x,x) - 1.0f;
+
+    float disc = b * b - 4 * c;
+    if (disc <= 0)
+        return 0;
+
+    return fmaxf(0.0f, (-b + sqrtf(disc)) / 2.0f);
+}
 
 __device__ Vector3float sampleHenyeyGreensteinPhase(const float& g, const Vector3float& forward, uint32_t& seed)
 {
@@ -295,6 +369,12 @@ __device__ Vector3float sampleHemisphereUniform(uint32_t& seed, const Vector3flo
     return Math::dot(result, N) < 0 ? -1.0f * result : result;
 }
 
+__device__ Vector3float sampleSphereUniform(uint32_t& seed)
+{
+    Vector3float result = sampleHemisphereUniform(seed, Vector3float(1, 0, 0));
+    return Math::rnd(seed) < 0.5 ? result : -1.0f * result;
+}
+
 __global__ void generateSamples(const uint32_t num_samples, Sphere sphere, PathSummary* buffer)
 {
     const uint32_t tid = ThreadHelper::globalThreadIndex();
@@ -304,12 +384,13 @@ __global__ void generateSamples(const uint32_t num_samples, Sphere sphere, PathS
         return;
     }
 
-    uint32_t seed = Math::tea<4>(tid, 1);
+    uint32_t seed = Math::tea<4>(tid, 0);
 
-    Vector3float start_pos = Vector3float(0.99, 0, 0); // Just inside the sphere
-    Vector3float direction = Vector3float(-1, 0, 0);// sampleHemisphereUniform(seed, Vector3float(-1, 0, 0));
+    Vector3float start_pos = Vector3float(0, 0, 0.99);
+    float theta = Math::rnd(seed)* static_cast<float>(M_PI) / 2.0f;
+    Vector3float direction = -1.0f * Vector3float(sinf(theta), 0, cosf(theta));
     Ray ray(start_pos, direction);
-    PathSummary summary = { direction, 0, 0 };
+    PathSummary summary = { start_pos, direction, Vector3float(0), Vector3float(0), 0 };
 
     //Volume
     float sigma_s = 3.0f;
@@ -317,35 +398,32 @@ __global__ void generateSamples(const uint32_t num_samples, Sphere sphere, PathS
     float sigma_t = sigma_s + sigma_a;
     float g = 0.6f;
 
-    Vector3float path[100];
-    path[0] = start_pos;
-
     while(true)
     {
-        Vector3float new_direction = sampleHenyeyGreensteinPhase(g, ray.direction(), seed);
-        ray.traceNew(start_pos + 0.001f * new_direction, new_direction);
+        ray.traceNew(start_pos + 0.001f * direction, direction);
 
-        LocalGeometry geom = sphere.computeRayIntersection(ray);
+        float d = distanceToBoundary(ray.origin(), ray.direction());
 
-        if (geom.depth == INFINITY)
+        if (d == INFINITY)
         {
-            summary.out_pos = start_pos;
-            summary.out_dir = new_direction;
+            summary.out_pos = ray.origin();
+            summary.out_dir = ray.direction();
             break;
         }
 
         float t = -logf(1.0f - Math::rnd(seed)) / sigma_t;
 
-        if (t >= geom.depth)
+        if (t >= d)
         {
             //printf("sample\n");
-            summary.out_pos = geom.P;
-            summary.out_dir = new_direction;
+            summary.out_pos = ray.origin() + d * ray.direction();
+            summary.out_dir = direction;
             break;
         }
 
         ++summary.num_scattering;
-        start_pos = start_pos + t * ray.direction();
+        start_pos = ray.origin() + t * ray.direction();
+        direction = sampleHenyeyGreensteinPhase(g, ray.direction(), seed);
         //path[summary.num_scattering] = start_pos;
     }
 
@@ -357,43 +435,66 @@ __global__ void generateSamples(const uint32_t num_samples, Sphere sphere, PathS
     buffer[tid] = summary;
 }
 
+__global__ void convertDataSet(const uint32_t N, PathSummary* buffer, DataPoint* dataset)
+{
+    const uint32_t tid = ThreadHelper::globalThreadIndex();
+
+    if(tid >= N)
+    {
+        return;
+    }
+
+    PathSummary summary = buffer[tid];
+
+    float theta_dir_in = acosf(-Math::clamp(summary.inc_dir.z, -1.0f, 1.0f));
+
+    float theta_pos_out = acosf(Math::clamp(summary.out_pos.z, -1.0f, 1.0f));
+    float phi_pos_out = atan2f(summary.out_pos.y, summary.out_pos.x);
+
+    float theta_dir_out = acosf(Math::clamp(summary.out_dir.z, -1.0f, 1.0f));
+    float phi_dir_out = atan2f(summary.out_dir.y, summary.out_dir.x);
+    
+    dataset[tid] = {theta_dir_in, summary.num_scattering, theta_pos_out, phi_pos_out, theta_dir_out, phi_dir_out};
+}
+
 void generateDataSet()
 {
-    const uint32_t N = 1 << 20;
+    const uint32_t N = 1 << 22;
     printf("Generating %i samples...\n", N);
 
     Sphere sphere(Vector3float(0, 0, 0), 1);
     
     PathSummary* buffer = Memory::createDeviceArray<PathSummary>(N);
+    DataPoint* dataset = Memory::createDeviceArray<DataPoint>(N);
 
     KernelSizeHelper::KernelSize config = KernelSizeHelper::configure(N);
     generateSamples << <config.blocks, config.threads >> > (N, sphere, buffer);
     ::cudaSafeCall(cudaDeviceSynchronize());
 
-    PathSummary* host_buffer = Memory::createHostArray<PathSummary>(N);
-    Memory::copyDevice2HostArray<PathSummary>(N, buffer, host_buffer);
-    Memory::destroyDeviceArray<PathSummary>(buffer);
+    convertDataSet << <config.blocks, config.threads >> > (N, buffer, dataset);
+    ::cudaSafeCall(cudaDeviceSynchronize());
+
+    DataPoint* host_dataset = Memory::createHostArray<DataPoint>(N);
+    Memory::copyDevice2HostArray<DataPoint>(N, dataset, host_dataset);
+    Memory::destroyDeviceArray<DataPoint>(dataset);
 
     std::ofstream file;
-    file.open("ScattersDataSetPositions.ds");
+    file.open("SphereScatters_Ver2.ds");
 
     for(uint32_t i = 0; i < N; ++i)
     {
-        file << host_buffer[i].inc_dir.x << ", " <<
-                host_buffer[i].inc_dir.y << ", " <<
-                host_buffer[i].inc_dir.z << ", " <<
-                host_buffer[i].num_scattering << ", " <<
-                host_buffer[i].out_pos.x << ", " <<
-                host_buffer[i].out_pos.y << ", " <<
-                host_buffer[i].out_pos.z << ", " <<
-                host_buffer[i].out_dir.x << ", " <<
-                host_buffer[i].out_dir.y << ", " <<
-                host_buffer[i].out_dir.z << "\n";
+        file << host_dataset[i].theta_dir_in << ", " <<
+                host_dataset[i].num_scattering << ", " <<
+                host_dataset[i].theta_pos_out << ", " <<
+                host_dataset[i].phi_pos_out << ", " <<
+                host_dataset[i].theta_dir_out << ", " <<
+                host_dataset[i].phi_dir_out << "\n";
     }
 
     file.close();
 
-    Memory::destroyHostArray<PathSummary>(host_buffer);
+    Memory::destroyHostArray<DataPoint>(host_dataset);
+    Memory::destroyDeviceArray<PathSummary>(buffer);
 }
 
 int run()
@@ -409,4 +510,4 @@ int main()
 {
     int exit = run();
     return exit;
-}*/
+}
