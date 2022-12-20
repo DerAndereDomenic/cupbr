00cffd7437dc535ebe6a153b9951496b3401ad30
Domenic Zingsheim <domeniczingsheim@gmail.com>
2022-12-20 14:00:50 +0100

    Initialize wavefront project

---
+++ src/Plugins/RendererWavefront/RendererWavefront.cu
@@ -0,0 +1,198 @@
+#include <CUPBR.h>
+
+namespace cupbr
+{
+
+    namespace detail
+    {
+        struct RadiancePayload
+        {
+            uint32_t seed;
+            Vector3float radiance = 0;
+            Vector3float rayweight = 1;
+            Vector3float ray_start;
+            Vector3float out_dir;
+            bool next_ray_valid;
+        };
+
+        CUPBR_DEVICE void directIllumination(Scene& scene, Ray& ray, LocalGeometry& geom, Vector3float& inc_dir)
+        {
+            //Direct illumination
+            RadiancePayload* payload = ray.payload<RadiancePayload>();
+
+            Vector3float normal = geom.N;
+
+            //Don't shade back facing geometry
+            if (geom.material->type != MaterialType::REFRACTIVE && Math::dot(normal, inc_dir) <= 0.0f)
+            {
+                //payload->rayweight = 0;
+                return;
+            }
+
+            uint32_t useEnvironmentMap = scene.useEnvironmentMap ? 1 : 0;
+            uint32_t light_sample = static_cast<uint32_t>(Math::rnd(payload->seed) * (scene.light_count + useEnvironmentMap));
+
+            Light light;
+            Vector3float lightDir, lightRadiance;
+            float d;
+            if (light_sample != scene.light_count)
+            {
+                light = *(scene.lights[light_sample]);
+
+                lightRadiance = light.sample(payload->seed, geom.P, lightDir, d);
+            }
+            else // Use environment map
+            {
+                Vector4float sample = geom.material->sampleDirection(payload->seed, inc_dir, geom.N);
+                lightDir = Vector3float(sample);
+                d = INFINITY; //TODO: Better way to do this
+                Vector2uint32_t pixel = Tracing::direction2UV(lightDir, scene.environment.width(), scene.environment.height());
+                lightRadiance = scene.environment(pixel) / sample.w;
+            }
+
+            Ray shadow_ray = Ray(geom.P + 0.001f * lightDir, lightDir);
+
+            if (Tracing::traceVisibility(scene, d, shadow_ray))
+            {
+                payload->radiance += (scene.light_count + useEnvironmentMap) *
+                    fmaxf(0.0f, Math::dot(normal, lightDir)) *
+                    geom.material->brdf(geom.P, inc_dir, lightDir, normal) *
+                    lightRadiance *
+                    payload->rayweight;
+            }
+        }
+
+        CUPBR_DEVICE void indirectIllumination(Ray& ray, LocalGeometry& geom, Vector3float& inc_dir)
+        {
+            RadiancePayload* payload = ray.payload<RadiancePayload>();
+
+            //Kind of back-face culling to handle back facing shading normals
+            if (geom.material->type != MaterialType::REFRACTIVE && Math::dot(geom.N, inc_dir) <= 0.0f)
+            {
+                payload->ray_start = geom.P;
+                payload->out_dir = -1.0f * inc_dir;
+                payload->next_ray_valid = true;
+                return;
+            }
+
+            //Indirect illumination
+            Vector4float direction_p = geom.material->sampleDirection(payload->seed, inc_dir, geom.N);
+            Vector3float direction = Vector3float(direction_p);
+            if (Math::norm(direction) == 0)
+                return;
+            ray.payload<RadiancePayload>()->rayweight = ray.payload<RadiancePayload>()->rayweight *
+                fabs(Math::dot(direction, geom.N)) *
+                geom.material->brdf(geom.P, inc_dir, direction, geom.N) / direction_p.w;
+            payload->out_dir = direction;
+            payload->ray_start = geom.P;
+            payload->next_ray_valid = true;
+        }
+
+        CUPBR_GLOBAL void
+        volume_kernel(Scene scene,
+                      const Camera camera,
+                      const uint32_t frameIndex,
+                      const uint32_t maxTraceDepth,
+                      const bool useRussianRoulette,
+                      Image<Vector3float> img)
+        {
+            const uint32_t tid = ThreadHelper::globalThreadIndex();
+
+            if (tid >= img.size())
+            {
+                return;
+            }
+
+            uint32_t seed = Math::tea<4>(tid, frameIndex);
+
+            Ray ray = Tracing::launchRay(tid, img.width(), img.height(), camera, true, &seed);
+            RadiancePayload payload;
+            payload.seed = seed;
+            ray.setPayload(&payload);
+
+            uint32_t trace_depth = 0;
+
+            Light light;
+
+            do
+            {
+                payload.next_ray_valid = false;
+                LocalGeometry geom;
+                    
+                geom = Tracing::traceRay(scene, ray);
+
+                Vector3float inc_dir = -1.0f * ray.direction(); //Points away from surface
+                if (geom.depth == INFINITY)
+                {
+                    if (scene.useEnvironmentMap)
+                    {
+                        Vector2uint32_t pixel = Tracing::direction2UV(ray.direction(), scene.environment.width(), scene.environment.height());
+                        payload.radiance += payload.rayweight * scene.environment(pixel);
+                    }
+                    break;
+                }
+
+                directIllumination(scene, ray, geom, inc_dir);
+                indirectIllumination(ray, geom, inc_dir);
+
+                if(useRussianRoulette)
+                {
+                    float alpha = Math::clamp(fmaxf(payload.rayweight.x, fmaxf(payload.rayweight.y, payload.rayweight.z)), 0.0f, 1.0f);
+                    if(Math::rnd(payload.seed) > alpha || Math::safeFloatEqual(alpha, 0))
+                    {
+                        payload.next_ray_valid = false;
+                        payload.rayweight = 0;
+                        break;
+                    }
+                    payload.rayweight = payload.rayweight / alpha;
+                }
+                    
+                ray.traceNew(payload.ray_start + 0.001f * payload.out_dir, payload.out_dir);
+                if (!payload.next_ray_valid)break;
+                ++trace_depth;
+            } while (trace_depth < maxTraceDepth);
+
+            if (frameIndex > 0)
+            {
+                const float a = 1.0f / (static_cast<float>(frameIndex) + 1.0f);
+                ray.payload<RadiancePayload>()->radiance = (1.0f - a) * img[tid] + a * ray.payload<RadiancePayload>()->radiance;
+            }
+
+            img[tid] = ray.payload<RadiancePayload>()->radiance;
+        }
+    } //namespace detail
+    
+    class RendererWavefront : public RenderMethod
+    {
+        public:
+
+            RendererWavefront(Properties* properties)
+        {
+            max_trace_depth = properties->getProperty("max_trace_depth", 5);
+            use_russian_roulette = properties->getProperty("use_russian_roulette", true);
+        }
+
+        virtual void 
+        render(Scene& scene,
+               const Camera& camera,
+               const uint32_t& frameIndex,
+               Image<Vector3float>* output_img) 
+        {
+            const KernelSizeHelper::KernelSize config = KernelSizeHelper::configure(output_img->size());
+            detail::volume_kernel << <config.blocks, config.threads >> > (scene,
+                                                                          camera,
+                                                                          frameIndex,
+                                                                          max_trace_depth,
+                                                                          use_russian_roulette,
+                                                                          *output_img);
+            synchronizeDefaultStream();
+        }
+        
+        private:
+        uint32_t max_trace_depth;
+        bool use_russian_roulette;
+    };
+
+    DEFINE_PLUGIN(RendererWavefront, "WavefrontRenderer", "1.0", RenderMethod)
+
+}
